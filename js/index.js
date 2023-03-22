3fdcd9d86009b94df638f829c8a38eae9e9265d3
Theilynalpha <tsaballoalphatservices.com>
2023-03-22 09:58:01 -0400

    esto es una prueba

---
+++ js/index.js
@@ -0,0 +1,13 @@
+/*Definiciones*/
+
+let clicked = [];
+let boardBlocked = false;
+let current_player= "X";
+
+let player_clicked = [];
+player_clicked["X"] =[];
+player_clicked["O"] =[];
+
+let player_counter = [];
+player_counter["X"] = 0;
+player_counter["0"] = 0;
